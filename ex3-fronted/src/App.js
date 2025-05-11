e1e3ab7da763f65cc5e44e339117f536429ccb05
Benjamin Rosin <benjmain.rosin@gmail.com>
2025-05-11 12:20:35 +0300

    merge

--- ex3-fronted/src/App.js
+++ ex3-fronted/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/*import Home from './components/Home';
 import Game from './components/Game';
 import Leaderboard from './components/Leaderboard';
 import WordManagement from './components/WordManagement';
-import About from './components/About';
+import About from './components/About';*/
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -16,22 +16,24 @@ import HomePage from "./components/HomePage";
  * @returns {JSX.Element} The main application component
  */
 function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container mt-4">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/game" element={<Game />} />
+    return (
+        <>
+            <BrowserRouter>
+                <div className="App">
+                <Navbar />
+                    <div className="container mt-4">
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            {/*<Route path="/game" element={<Game />} />
               <Route path="/leaderboard" element={<Leaderboard />} />
               <Route path="/admin" element={<WordManagement />} />
-              <Route path="/about" element={<About />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-  );
+              <Route path="/about" element={<About />} />*/}
+                        </Routes>
+                    </div>
+                </div>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
