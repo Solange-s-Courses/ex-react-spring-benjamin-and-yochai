e91151d3da83e9ff2a0e1d058d37f04b26233787
Yochai <yochai.benita@gmail.com>
2025-05-11 10:53:29 +0300

    start

---
+++ src/main/java/com/example/ex3reactspringbenjaminandyochai/Ex3ReactSpringBenjaminAndYochaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex3reactspringbenjaminandyochai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex3ReactSpringBenjaminAndYochaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex3ReactSpringBenjaminAndYochaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ex3reactspringbenjaminandyochai/Ex3ReactSpringBenjaminAndYochaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex3reactspringbenjaminandyochai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex3ReactSpringBenjaminAndYochaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
